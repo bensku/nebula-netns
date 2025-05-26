41f4cf895edf2460fff1c5434be03c635206de49
bensku <bmi@iki.fi>
2025-05-26 23:26:32 +0300

    Add version number to builds

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+version="1.9.5-netns0"
+
 export GOOS=linux # We're using Linux network namespaces, which are by definition platform-specific
-GOARCH=amd64 go build -o nebula-netns-linux-amd64
-GOARCH=arm64 go build -o nebula-netns-linux-arm64
+GOARCH=amd64 go build -o nebula-netns-linux-amd64 -ldflags "-X main.Build=$version"
+GOARCH=arm64 go build -o nebula-netns-linux-arm64 -ldflags "-X main.Build=$version"
